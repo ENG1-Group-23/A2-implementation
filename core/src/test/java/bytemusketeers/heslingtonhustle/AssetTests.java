187880ce104dc179d83ad4f6b1926d7455747334
Prajwal-D <prajwal.dethekar@gmail.com>
2024-05-11 14:33:59 +0100

    Segmented tests into more reasonable categories

--- core/src/test/java/bytemusketeers/heslingtonhustle/AssetTests.java
+++ core/src/test/java/bytemusketeers/heslingtonhustle/AssetTests.java
@@ -28,35 +28,47 @@ public class AssetTests {
     }
 
     @Test
-    public void testAssetsExist() {
-        //There has to be a better way than me looking at the files this program opens
-        //Perhaps have a constants file with all the assets we use? And then whenever
-        //a file's path is needed, we take it from the constants?
-
+    public void tilemapAssetsExist() {
         //Maps
         assertTrue("The main map tilemap asset exists", Gdx.files.internal("map/MainMap.tmx").exists());
+    }
 
+    @Test
+    public void fontAssetsExist() {
         //Fonts
         assertTrue("The main font asset exists", Gdx.files.internal("font/WhitePeaberry.fnt").exists());
+    }
 
+    @Test
+    public void soundAssetsExist() {
         //Sounds
         assertTrue("The BGM asset exists", Gdx.files.internal("music_loop/Ludum Dare 30 - 01.ogg").exists());
         assertTrue("Volume up sound asset exists", Gdx.files.internal("sfx/high_note.mp3").exists());
         assertTrue("Volume down sound asset exists", Gdx.files.internal("sfx/low_note.mp3").exists());
         assertTrue("Button press sound asset exists", Gdx.files.internal("sfx/button_press.mp3").exists());
         assertTrue("Eating sound asset exists", Gdx.files.internal("sfx/eating_sound.wav").exists()); //why is this a wav
+    }
 
-        //Textures
+    @Test
+    public void controlsScreenTexturesExist() {
         //Controls screen
         assertTrue("Back button(Controls) asset exists", Gdx.files.internal("settings_gui/back_button.png").exists());
         assertTrue("Labels for controls asset exists", Gdx.files.internal("controls_gui/controls_label.png").exists());
         assertTrue("Controls asset exists", Gdx.files.internal("controls_gui/controls.png").exists());
+    }
+
+    @Test
+    public void menuScreenTexturesExist() {
         //Menu screen
         assertTrue("HeslingtonHustle label asset exists", Gdx.files.internal("menu_gui/heslington_hustle_label.png").exists());
         assertTrue("Play button asset exists", Gdx.files.internal("menu_gui/play_button.png").exists());
         assertTrue("Controls button asset exists", Gdx.files.internal("menu_gui/controls_button.png").exists());
         assertTrue("Settings button asset exists", Gdx.files.internal("menu_gui/settings_button.png").exists());
         assertTrue("Exit button asset exists", Gdx.files.internal("menu_gui/exit_button.png").exists());
+    }
+
+    @Test
+    public void gameScreenTexturesExist() {
         //Game screen
         assertTrue("Menu button asset exists", Gdx.files.internal("menu_buttons/menu_icon.png").exists());
         assertTrue("Stats background asset exists", Gdx.files.internal("counter_background.png").exists());
@@ -69,13 +81,25 @@ public class AssetTests {
         assertTrue("Study button asset exists", Gdx.files.internal("study_button.png").exists());
         assertTrue("Sleep button asset exists", Gdx.files.internal("sleep_button.png").exists());
         assertTrue("Exercise go button asset exists", Gdx.files.internal("go_button.png").exists());
-        assertTrue("Energy assets exists", energyAssets()); //there's like 10
+        assertTrue("Energy assets exists", energyAssets());
+    }
+
+    @Test
+    public void minigameTexturesExist() {
         //Minigame
         assertTrue("Guess button asset exists", Gdx.files.internal("mini_games/guess_button.png").exists());
         assertTrue("Memoriser label asset exists", Gdx.files.internal("mini_games/number_memoriser_label.png").exists());
+        // NEED TO ADD DUCK HUN- FEEDING MINIGAME HERE
+    }
+
+    @Test
+    public void endScreenTexturesExist() {
         //EndScreen
         assertTrue("Play again button asset exists", Gdx.files.internal("end_gui/play_button.png").exists());
         assertTrue("Exit button asset exists", Gdx.files.internal("end_gui/exit_button.png").exists());
+    }
+    @Test
+    public void settingsScreenTexturesExist() {
         //Settings
         //back button already tested
         assertTrue("Back button(Settings) asset exists", Gdx.files.internal("settings_gui/back_button.png").exists());
@@ -89,7 +113,9 @@ public class AssetTests {
         assertTrue("Music/sound bar assets exists", volumeBarAssets());
         assertTrue("Boy selection assets exists", Gdx.files.internal("settings_gui/boy_button.png").exists() & Gdx.files.internal("settings_gui/boy_button_indented.png").exists());
         assertTrue("Girl selection assets exists", Gdx.files.internal("settings_gui/girl_button.png").exists() & Gdx.files.internal("settings_gui/girl_button_indented.png").exists());
-
+    }
+    @Test
+    public void playerTexturesExist() {
         //Player
         assertTrue("Boy idle asset exists", Gdx.files.internal("character/boy_idle.png").exists());
         assertTrue("Boy walking asset exists", Gdx.files.internal("character/boy_walk.png").exists());
